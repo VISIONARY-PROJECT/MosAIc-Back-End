fd147f8e362c85402e5e8b6e152705f1667c28a5
민재준 <minjaejun291@gmail.com>
2024-06-19 00:59:50 +0900

    서버 9-1(세션기능업데이트)

--- app.py
+++ app.py
@@ -5,10 +5,16 @@ import face_model
 import datetime
 from flask_cors import CORS
 
+
+
 app = Flask(__name__)
 CORS(app)
 CORS(app,resource={r'*':{'origins':'*'}})
 app.config["SECRET_KEY"] = "dasggasdgasd"
+
+app.config["SESSION_COOKIE_SAMESITE"] = "None"
+app.config["SESSION_COOKIE_SECURE"] = True
+
 DB=DBmodule()
 
 @app.route("/")                     #홈화면 버튼에 대한 처리(로그인o : 업로드 화면, 로그인x : 로그인 화면으로)
